4fd7c79e99011a9d51ba5bc5605561fc8fcff30c
y-tikhenkiy <y.tikhenkiy@gmail.com>
2024-03-13 15:11:38 +0200

    added faker and tuned reg test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import {faker} from '@faker-js/faker'
 
 test('has title "Contact List App"', async ({ page }) => {
   await page.goto('./');
@@ -15,12 +16,19 @@ test('has title "Add User"', async ({ page }) => {
 });
 
 test('should be able to "Add User"', async ({ page }) => {
+let firstName: string = faker.person.firstName();
+let lastName: string = faker.person.lastName();
+let email: string = faker.internet.email();
+let userPass: string = faker.internet.password({length:8})
+
+console.log(`first name:\t${firstName} \nlast name:\t${lastName} \nemail:\t\t${email} \npassword:\t${userPass}`);
+
   await page.goto('./');
   await page.locator('#signup').click();
-  await page.locator('#firstName').fill("test");
-  await page.locator('#lastName').fill("test");
-  await page.locator('#email').fill("test031324@test.com");
-  await page.locator('#password').fill("1234567");
+  await page.locator('#firstName').fill(firstName);
+  await page.locator('#lastName').fill(lastName);
+  await page.locator('#email').fill(email);
+  await page.locator('#password').fill(userPass);
   await page.locator('#submit').click();
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle('My Contacts');
